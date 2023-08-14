efd2ec35bf08deca288f2d9a80d959a09b0c9dde
Carlos Lopez <cgl56783@gmail.com>
2023-08-14 13:23:04 -0300

    update

--- Campeones/models.py
+++ Campeones/models.py
@@ -4,5 +4,5 @@ import datetime
 class Campeones(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
-    imagenes = models.ImageField(upload_to="media/Campeones_img/images")
+    imagenes = models.ImageField(upload_to="Campeones_img/images")
     date = models.DateField(datetime.date.today)
